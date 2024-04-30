06aa5a65e1427b87aa7b8999a1a28b3463e7f5d3
noberumotto <root@thelittlepandaisbehind.com>
2024-05-01 04:27:55 +0800

    添加文本输入框控件

---
+++ LancerUI/Controls/Input/LUInputCommands.cs
@@ -0,0 +1,15 @@
+﻿using LancerUI.Controls.Windows;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace LancerUI.Controls.Input
+{
+    public class LUInputCommands
+    {
+        public static RoutedUICommand ClearableCommand { get; } = new RoutedUICommand("", "ClearableCommand", typeof(LUInputCommands));
+    }
+}
